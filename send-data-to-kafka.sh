9dddb321697cd754210339cfa8d69cfac35e5c01
hoseinlook <hoseinlooki0@gmail.com>
2022-09-03 12:14:23 +0430

    add variable KAFKA_TOPIC

--- send-data-to-kafka.sh
+++ send-data-to-kafka.sh
@@ -1,8 +1,11 @@
+
 BASH_SOURCE=$0
 ROOT_OF_PROJECT=$(dirname ${BASH_SOURCE})
 PATH_OF_DATA="$ROOT_OF_PROJECT/storage/data/cpu_usage.data"
 KAFKA_HOME="$ROOT_OF_PROJECT/kafka/bin"
-$KAFKA_HOME/kafka-topics.sh -topic test -create --bootstrap-server localhost:9093
+KAFKA_TOPIC=cpu-usage
+
+$KAFKA_HOME/kafka-topics.sh -topic ${KAFKA_TOPIC} -create --bootstrap-server localhost:9093
 echo "sending data ..."
-tail -n +2 $PATH_OF_DATA | $KAFKA_HOME/kafka-console-producer.sh --topic test  --bootstrap-server localhost:9093
+tail -n +2 $PATH_OF_DATA | $KAFKA_HOME/kafka-console-producer.sh --topic test --bootstrap-server localhost:9093
 echo "finished"
